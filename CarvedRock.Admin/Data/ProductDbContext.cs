c2dda52c7b1e252c07baef0d6eb902bf3f38215f
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-10-19 12:10:22 +0200

    Add the productDbContext for data fetching from DB

---
+++ CarvedRock.Admin/Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CarvedRock.Admin.Data;
+
+public class ProductDbContext : DbContext
+{
+    public DbSet<Product> Products => Set<Product>();
+
+    public string DbPath { get; set; }
+
+    public ProductDbContext()
+    {
+        var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        DbPath = Path.Join(path, "carved-rock.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+}
