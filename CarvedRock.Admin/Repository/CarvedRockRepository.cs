e5613c6c5a7748c479548c7e4dd3fcb8b8a0ddda
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-10-24 12:11:12 +0200

    Add a repository for Product entity

---
+++ CarvedRock.Admin/Repository/CarvedRockRepository.cs
@@ -0,0 +1,58 @@
+using CarvedRock.Admin.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarvedRock.Admin.Repository;
+
+public class CarvedRockRepository : ICarvedRockRepository
+{
+    private readonly ProductDbContext _productDbContext;
+    public CarvedRockRepository(ProductDbContext productDbContext)
+    {
+        _productDbContext = productDbContext;
+    }
+    public async Task<Product> AddProductAsync(Product product)
+    {
+        _productDbContext.Products.Add(product);
+        await _productDbContext.SaveChangesAsync();
+        return product; // will have an updated ID value?
+    }
+
+    public async Task<List<Product>> GetAllProductsAsync()
+    {
+        return await _productDbContext.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetProductByIdAsync(int productId)
+    {
+        return await _productDbContext.Products.FirstOrDefaultAsync( p => p.Id == productId);
+    }
+
+    public async Task RemoveProductAsync(int productIdToRemove)
+    {
+        var product = await _productDbContext.Products.FirstOrDefaultAsync(p => p.Id == productIdToRemove);
+        if(product != null)
+        {
+            _productDbContext.Products.Remove(product);
+            await _productDbContext.SaveChangesAsync();
+        }
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        try
+        {
+            _productDbContext.Update(product);
+            await _productDbContext.SaveChangesAsync();
+        }
+        catch(DbUpdateConcurrencyException e)
+        {
+            if(_productDbContext.Products.Any(p => p.Id == product.Id))
+            {
+                // product exists and update exception is real
+                throw;
+            }
+            // caught and swallowed exception can occur if 
+            // the other update was a delete operation
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Prerequisites
 
-* Install the [.NET SDK](https://dotnet.microsoft.com/en-us/download)
+* Install the [.NET SDK](https://dotnet.microsoft.com/en-us/download).
 * Have installed the following IDEs:
   * the [VS Code](https://code.visualstudio.com) including the [C# language extension](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp)
   * the [JetBrains Rider](https://www.jetbrains.com/rider/)
@@ -11,13 +11,13 @@
 
 ## The App Features
 
-This project is about building a web application for a fictional company named **Carved Rock** for its employee to use as an administration tool 
-to maintain the products it sells and their categories they belong to. For that purpose, many technologies are used:
+This project is about building a web application for a fictional company named **Carved Rock** for its employee to use as an administration tool to maintain the products it sells and their categories they belong to. For that purpose, many technologies are used:
 
 * The primary technology used in our project is **ASP.NET Core MVC 7**.
 * **Entity Framework (EF) Core 7** is used with SQLite for database.
 * Basic CRUD operations are supported on Products.
-* Bootstrap 5 is used for styling the web application.
+* Repository classes are used with **Dependency Injection (DI)** for testability support.
+* **Bootstrap 5** is used for styling the web application.
 
 ## Documentation References
 
