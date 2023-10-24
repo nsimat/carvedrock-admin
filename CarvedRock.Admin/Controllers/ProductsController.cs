3f7cfea3f378e2f551fc267cfa0e5805c2c80cee
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-10-24 16:08:37 +0200

    Refactoring the ProductsController

--- CarvedRock.Admin/Controllers/ProductsController.cs
+++ CarvedRock.Admin/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using CarvedRock.Admin.Logic;
 using CarvedRock.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,36 +6,39 @@ namespace CarvedRock.Admin.Controllers;
 
 public class ProductsController : Controller
 {
-    public List<ProductModel> Products { get; set; }
+    private readonly IProductLogic _logic;
+    //public List<ProductModel> Products { get; set; }
 
-    public ProductsController()
+    public ProductsController(IProductLogic logic)
     {
-        Products = GetSampleProducts();
+        //Products = GetSampleProducts();
+        _logic = logic;
     }    
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View(Products);
+        var products = await _logic.GetAllProducts();
+        return View(products);
     }
 
-    public IActionResult Details(int id)
+    public async Task<IActionResult> Details(int id)
     {
-        var product = Products.Find(p => p.Id == id);
+        var product = await _logic.GetProductById(id);
         return product == null ? NotFound() : View(product);
     }
 
-    private List<ProductModel>? GetSampleProducts()
-    {
-        return new List<ProductModel>
-        {
-            new ProductModel {Id = 1, Name = "Trailblazer", Price = 69.99M, IsActive = true,
-                Description = "Great support in this high-top to take you to great heights and trails." },
-            new ProductModel {Id = 2, Name = "Coastliner", Price = 49.99M, IsActive = true,
-                Description = "Easy in and out with this lightweight but rugged shoe with great ventilation to get your around shores, beaches, and boats."},
-            new ProductModel {Id = 3, Name = "Woodsman", Price = 64.99M, IsActive = true,
-                Description = "All the insulation and support you need when wandering the rugged trails of the woods and backcountry." },
-            new ProductModel {Id = 4, Name = "Basecamp", Price = 249.99M, IsActive = true,
-                Description = "Great insulation and plenty of room for 2 in this spacious but highly-portable tent."},
-        };
-    }
+    // private List<ProductModel>? GetSampleProducts()
+    // {
+    //     return new List<ProductModel>
+    //     {
+    //         new ProductModel {Id = 1, Name = "Trailblazer", Price = 69.99M, IsActive = true,
+    //             Description = "Great support in this high-top to take you to great heights and trails." },
+    //         new ProductModel {Id = 2, Name = "Coastliner", Price = 49.99M, IsActive = true,
+    //             Description = "Easy in and out with this lightweight but rugged shoe with great ventilation to get your around shores, beaches, and boats."},
+    //         new ProductModel {Id = 3, Name = "Woodsman", Price = 64.99M, IsActive = true,
+    //             Description = "All the insulation and support you need when wandering the rugged trails of the woods and backcountry." },
+    //         new ProductModel {Id = 4, Name = "Basecamp", Price = 249.99M, IsActive = true,
+    //             Description = "Great insulation and plenty of room for 2 in this spacious but highly-portable tent."},
+    //     };
+    // }
 }
