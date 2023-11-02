81d1664e9a797ea66890726cc64d1cdac6a3addd
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-11-02 21:36:51 +0100

    Adding Category entity, table & relationship

--- CarvedRock.Admin/Data/ProductDbContext.cs
+++ CarvedRock.Admin/Data/ProductDbContext.cs
@@ -5,6 +5,7 @@ namespace CarvedRock.Admin.Data;
 public class ProductDbContext : DbContext
 {
     public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
 
     public string DbPath { get; set; }
 
@@ -25,10 +26,20 @@ public class ProductDbContext : DbContext
             SaveChanges();
         }
 
+        if(Categories.Any())
+        {
+            Categories.RemoveRange(Categories);
+            SaveChanges();
+        }
+
+        var footwearCat = new Category {Id = 1000, Name = "Footwear" };
+        var equipmentCat = new Category {Id = 2000, Name = "Equipment" };
+
         Products.Add(new Product
         {
             Id = 1,
             Name = "Trailblazer",
+            Category = footwearCat,
             Price = 69.99M,
             IsActive = true,
             Description = "Great support in this high-top to take you to great heights and trails."
@@ -37,6 +48,7 @@ public class ProductDbContext : DbContext
         {
             Id = 2,
             Name = "Coastliner",
+            Category = footwearCat,
             Price = 49.99M,
             IsActive = true,
             Description = "Easy in and out with this lightweight but rugged shoe with great ventilation to get your around shores, beaches, and boats."
@@ -45,6 +57,7 @@ public class ProductDbContext : DbContext
         {
             Id = 3,
             Name = "Woodman",
+            Category = footwearCat,
             Price = 64.99M,
             IsActive = true,
             Description = "All the insulation and support you need when wandering the rugged trails of the woods and backcountry."
@@ -53,10 +66,14 @@ public class ProductDbContext : DbContext
         {
             Id = 4,
             Name = "Basecamp",
+            Category = equipmentCat,
             Price = 249.99M,
             IsActive = true,
             Description = "Great insulation and plenty of room for 2 in this spacious but highly-portable tent."
         });
+
+        Categories.Add(footwearCat);
+        Categories.Add(equipmentCat);
 
         SaveChanges();
     }
